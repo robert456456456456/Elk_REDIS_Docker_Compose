5be7086f960a669c18bb87d1333bbbbcc3b2005f
C5268168 <robert.adinihu@sap.com>
2018-07-12 10:55:17 +0300

    version one

---
+++ health_chek.py
@@ -0,0 +1,13 @@
+import urllib2
+import os
+try:
+    request = urllib2.Request('localhost:9200')
+    response = urllib2.urlopen(request)
+except:
+    print 'faild '
+
+if "200" in str(response.getcode()):
+    print 'ok'
+
+else:
+    os.system("docker-compose stop;docker rm $(docker ps -a -q);docker rmi $(docker images -q);docker-compose up -d;")
