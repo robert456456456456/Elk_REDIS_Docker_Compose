1cf17c22b8c8b220fd4fcfbe6af8fc26bd181fa6
C5268168 <robert.adinihu@sap.com>
2018-07-12 11:07:14 +0300

    version one

--- health_chek.py
+++ health_chek.py
@@ -4,11 +4,13 @@ response=''
 try:
     request = urllib2.Request('localhost:9200')
     response = urllib2.urlopen(request)
+    print response.getcode()
 except:
     print 'faild '
 
-if "200" in str(response.getcode()):
+if response.getcode():
     print 'ok'
 
 else:
+    print 'inside else'
     os.system("docker-compose stop;docker rm $(docker ps -a -q);docker rmi $(docker images -q);docker-compose up -d;")
