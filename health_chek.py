6ac92d34e7ee95a616d1c39112a631cc05286adb
C5268168 <robert.adinihu@sap.com>
2018-07-12 11:24:23 +0300

    version one

--- health_chek.py
+++ health_chek.py
@@ -1,16 +1,13 @@
 import urllib2
 import os
-response=''
 try:
-    request = urllib2.Request('localhost:9200')
-    response = urllib2.urlopen(request)
-    print response.getcode()
+    response = urllib2.urlopen('http://localhost:9200/')
+
+    if response.code == 200:
+        print 'ok'
+    else:
+        print 'inside else'
+        os.system("docker-compose stop;docker rm $(docker ps -a -q);docker rmi $(docker images -q);docker-compose up -d;")
 except:
-    print 'faild '
-
-if response.getcode():
-    print 'ok'
-
-else:
-    print 'inside else'
     os.system("docker-compose stop;docker rm $(docker ps -a -q);docker rmi $(docker images -q);docker-compose up -d;")
+
